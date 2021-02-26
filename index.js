b6a7ff80f97116af95a1aec0c9cad648f3f0a6c6
Brendan Martin <bmartin@relaynetwork.com>
2021-02-26 14:49:55 -0500

    Intial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+/**
+ * Fake API call w/ custom timeout
+ * @param time
+ * @returns {Promise<unknown>}
+ */
+const fakeApiCall = async (time) => {
+  return new Promise((resolve) => {
+    return setTimeout(() => {
+      const message = `fakeApiCall finished in ${time}ms`;
+      console.log(message);
+      resolve(message);
+    }, time);
+  });
+};
+
+/**
+ * calling multiple promises
+ */
+const allSettledTest = async () => {
+  try {
+    const t = await Promise.allSettled([fakeApiCall(1000), fakeApiCall(10)]);
+    console.log(t);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ * while loop w/ promises
+ */
+const whileLoopTest = async () => {
+  let x = 0;
+  while (x < 5) {
+    x++;
+    await Promise.allSettled([fakeApiCall(1000), fakeApiCall(10)]);
+  }
+};
+
+/**
+ * catching error
+ */
+const catchErrorTest = () => {
+  const p = Promise.reject('no');
+  p.catch((e) => {
+    console.error(e);
+    throw e;
+  }).catch(e => console.error(e));
+};
+
+/**
+ * catch and throw
+ * @returns {Promise<void>}
+ */
+const catchErrorTest2 = async () => {
+  const p = Promise.reject('no');
+  try {
+    await p;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+/**
+ * catch and throw and catch
+ * @returns {Promise<void>}
+ */
+const catchErrorTest3 = async () => {
+  const p = Promise.reject('no');
+  try {
+    try {
+      await p;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ *
+ * @returns {Promise<void>}
+ */
+const promiseAllTest = async () => {
+  try {
+    const t = await Promise.all([Promise.reject('no'), Promise.resolve('yes')]);
+    console.log(t);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ *
+ * @returns {Promise<void>}
+ */
+const promiseAllSettledTest = async () => {
+  try {
+    const t = await Promise.allSettled([Promise.reject('no'), Promise.resolve('yes')]);
+    console.log(t);
+  } catch (e) {
+    console.error(e);
+  }
+};
